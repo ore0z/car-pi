f1900c4cd36b852ea57e543444e84cd0cef93fcf
Matt John <mjohn@zendesk.com>
2019-01-24 22:03:12 -0600

    update timestamp and ISO in record file

--- record-cam.py
+++ record-cam.py
@@ -5,7 +5,7 @@ import time
 import subprocess
 import os
 
-time.sleep(10) # Wait a bit for system to finish booting
+#time.sleep(10) # Wait a bit for system to finish booting
 
 wait = 300 #Duration (seconds) of each video file
 ext = '.h264' #RAW video file extention
@@ -13,15 +13,16 @@ ext = '.h264' #RAW video file extention
 with picamera.PiCamera() as camera:
     #camera.start_preview() # Testing only
     camera.resolution = (1640, 922) # Best resolution for pi cam v2 at full FOV
-    #camera.framerate = 35
-    #camera.iso = 800
+    #camera.resolution = (1280, 720) # Not full FOV
+    camera.framerate = 30
+    camera.iso = 800
     camera.exposure_mode = 'auto'
     camera.rotation = 180
     camera.annotate_text_size = 10
     camera.annotate_background = picamera.Color('black')
-    ts = dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    ts = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
     filename = ts + ext
-    camera.start_recording('/video/' + filename)
+    camera.start_recording('/video/' + filename, bitrate=5000000)
     start = dt.datetime.now()
     while (dt.datetime.now() - start).seconds < wait:
         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -32,7 +33,7 @@ with picamera.PiCamera() as camera:
             /video/{}.mp4 && rm /video/{}".format(filename, ts, filename))
     subprocess.Popen(["bash", "-c", cmd])
     while True:
-        ts = dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        ts = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
         filename = ts + ext
         camera.split_recording('/video/' + filename)
         start = dt.datetime.now()
